d7e7521f1084356c9fb2af28f990575686019996
msolorio <msolorio789@gmail.com>
2017-04-21 09:16:13 -0700

    update to use promise methods: done, fail, and always

--- js/app.js
+++ js/app.js
@@ -13,17 +13,12 @@
   // UPDATE DATA
   ////////////////////////////////////////////////////////////////////
   function updateData(dataFromApi, dom, dataStore) {
-    console.log('dataFromApi.items:', dataFromApi.items);
     dataStore.items = dataFromApi.items.map(function(item) {
       var dataStoreItem = {};
       dataStoreItem.imageUrl = item.snippet.thumbnails.high.url;
       dataStoreItem.videoUrl = item.id.videoId;
       return dataStoreItem;
     });
-    // is there a way we can call this within renderState instead?
-    // we're doing it here as a callback to getData to make sure we have the data
-    // before trying to render
-    displayResults(dom, dataStore);
   };
 
   ////////////////////////////////////////////////////////////////////
@@ -38,49 +33,46 @@
         q: state.searchTerm
       },
       dataType: 'json',
-      type: 'GET',
-      success: function(dataFromApi) { updateData(dataFromApi, dom, dataStore) },
-      error: function() {
-        console.log('there was an error with your request');
-        // was trying to figure out how to catch an error on an ajax request
-        // I'd rather call the renderMessage function within renderState rather than here
-        // however if there is an uncaught error code execution would stop and we
-        // would never get there
-        updateMessage(state, 'There was an error with your request');
-        renderMessage(dom, state);
-      }
+      type: 'GET'
     };
 
-    $.ajax(settings);
+    $.ajax(settings)
+      .done(function(dataFromApi) {
+        updateData(dataFromApi, dom, dataStore);
+      })
+      .fail(function() {
+        // did not call update state here bc getData is called inside update state
+        // we only want to update the message here
+        updateMessage(state, 'There was an error with your request');
+      })
+      .always(function() {
+        renderState(dom, state);
+      });
   };
 
-  function updateSearchTerm(state, searchTerm) {
-    state.searchTerm = searchTerm;
+  function updateSearchTerm(dom, state) {
+    state.searchTerm = $(dom.searchInput).val().trim();
   }
 
   function updateMessage(state, message) {
     state.message = message;
   };
 
-  // when is it a good idea to pass in callbacks as parameters?
-  // and why would you do that if they are available in scope?
-  // here we're calling a lot of different functions conditionally
   function validateInput(dom, state, dataStore) {
-    var searchTerm = $(dom.searchInput).val().trim();
-    if (searchTerm === '') {
-      updateSearchTerm(state, searchTerm);
-      updateMessage(state, 'Please enter a search term');
-      dataStore.items = null;
-    } else {
-      updateSearchTerm(state, searchTerm);
-      updateMessage(state, '');
-      getData(dom, state, dataStore, updateData);
-    }
+    return $(dom.searchInput).val().trim() === '' ? false : true;
   };
 
   function updateState(dom, state, dataStore) {
-    validateInput(dom, state, dataStore);
-    console.log('state:', state);
+    updateSearchTerm(dom, state);
+    if (validateInput(dom, state, dataStore)) {
+      updateMessage(state, '');
+      // getData will call renderState as it's callback
+      getData(dom, state, dataStore, updateData);
+    } else {
+      updateMessage(state, 'Please enter a search term');
+      dataStore.items = null;
+      renderState(dom, state);
+    }
   };
 
   //////////////////////////////////////////////////////////////////
@@ -96,7 +88,9 @@
           </div>'
         );
       });
-      $('.results').html(results);
+      $(dom.results).html(results);
+    } else {
+      $(dom.results).html('');
     }
   };
 
@@ -112,7 +106,7 @@
   function renderState(dom, state) {
     renderMessage(dom, state);
     clearInput(dom);
-    // displayResults(dom, dataStore);
+    displayResults(dom, dataStore);
   };
 
   //////////////////////////////////////////////////////////////////
@@ -138,14 +132,6 @@
     $(dom.button).click(function(event) {
       event.preventDefault();
       updateState(dom, state, dataStore);
-      renderState(dom, state);
-    });
-  };
-
-  // may remove if we don't need it
-  function listenForAjaxError() {
-    $(document).ajaxError(function() {
-      console.log('there was an error with your request');
     });
   };
 
@@ -162,7 +148,6 @@
     dom.videoImage = '.video_image';
 
     listenForFormSubmit(dom, state);
-    listenForAjaxError();
     listenForThumbnailClick(dom);
   });
 
